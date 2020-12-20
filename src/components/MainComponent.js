c89d01ff666100be8699165fb00bba9a6f88e9a2
maxvaart <maximiliano_lain3@hotmail.com>
2020-12-19 22:35:19 -0300

    Second Assignment

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,6 +9,7 @@ import {LEADERS} from '../shared/leaders';
 import Header from './HeaderComponent';
 import Contact from './ContactComponent';
 import Footer from './FooterComponent';
+import About from './AboutComponent';
 import {Switch, Route, Redirect} from 'react-router-dom';
 
 class Main extends Component {
@@ -44,7 +45,7 @@ class Main extends Component {
         ></Home>
       )
     }
-    
+
   return (
     <div>
       <Header></Header>
@@ -53,6 +54,7 @@ class Main extends Component {
         <Route exact path="/menu" component={()=><Menu dishes={this.state.dishes}></Menu>}></Route>
         <Route exact path="/contactus" component={Contact}></Route>
         <Route path='/menu/:dishId' component={DishWithId}></Route>
+        <Route exact path='/aboutus' component={()=><About leaders={this.state.leaders}></About>}></Route>
         <Redirect to="/home"></Redirect>
       </Switch>
       <Footer></Footer>
