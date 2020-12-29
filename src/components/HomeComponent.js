64bcc43894bc4c53eb51b9cb7e0cbe1774412a4d
maxvaart <maximiliano_lain3@hotmail.com>
2020-12-29 19:08:57 -0300

    Fetch to server

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,6 +1,7 @@
 import react from 'react';
 import {Card, CardImg,CardText,CardBody,CardTitle,CardSubtitle} from 'reactstrap';
 import {Loading} from './LoadingComponent';
+import {baseUrl} from '../shared/baseUrl';
 
 function RenderCard({item, isLoading, errMess}){
     if (isLoading){
@@ -15,7 +16,7 @@ function RenderCard({item, isLoading, errMess}){
     } else {
     return(
         <Card>
-            <CardImg src={item.image} alt={item.name}></CardImg>
+            <CardImg src={baseUrl + item.image} alt={item.name}></CardImg>
             <CardBody>
                 <CardTitle>{item.name}</CardTitle>
                 {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null}
@@ -34,7 +35,7 @@ function Home(props){
                     <RenderCard item={props.dish} isLoading={props.dishesLoading} errMess={props.dishesErrMess}></RenderCard>
                 </div>
                 <div className="col-12 col-md m-1">
-                    <RenderCard item={props.promotion}></RenderCard>
+                    <RenderCard item={props.promotion} isLoading={props.promoLoading} errMess={props.promoErrMess}></RenderCard>
                 </div>
                 <div className="col-12 col-md m-1">
                     <RenderCard item={props.leader}></RenderCard>
--- src/redux/comments.js
+++ src/redux/comments.js
@@ -1,14 +1,19 @@
-import {COMMENTS} from '../shared/comments';
 import * as ActionTypes from './ActionTypes';
 
-export const Comments= (state = COMMENTS, actions) =>{
+export const Comments= (state = {
+    errMess: null,
+    comments:[]}, actions) =>{
     switch(actions.type){
+        case ActionTypes.ADD_COMMENTS:
+            return{...state, isLoading: false, errMess:null, comments:actions.payload};
+        case ActionTypes.COMMENTS_FAILED:
+            return{...state, isLoading: false, errMess:actions.payload};
         case ActionTypes.ADD_COMMENT:
             var comment =  actions.payload;
             comment.id = state.length;
             comment.date = new Date().toISOString();
             console.log("Comment: ", comment);
-            return state.concat(comment);
+            return {...state, comments: state.comments.concat(comment)};
         default:
             return state;
     }
